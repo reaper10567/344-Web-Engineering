08d1266c1b0472f5b13eb403fb177808c077de7c
Tyler Gerber <tcgerber10567@gmail.com>
2015-09-27 19:35:38 -0400

    extremely basic html stoofs

---
+++ src/SE344/Controllers/StocksController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace SE344.Controllers
+{
+    public class StocksController : Controller
+    {
+        
+        public IActionResult stocks()
+        {
+            return View();
+        }
+    }
+}
+
+
