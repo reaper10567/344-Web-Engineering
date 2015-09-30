c0ef028009ce7b1920b570585bff93b6d6534706
Kai <kch2100@rit.edu>
2015-09-30 11:26:45 -0400

    Setup a Test Project for unit testing
    Using xUnit 2 as the test framework

---
+++ src/SE344Test/Models/StockTransactionTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SE344.Models;
+using Xunit;
+
+namespace SE344Test.Models
+{
+    public class StockTransactionTest
+    {
+        [Fact]
+        public void TotalPriceShouldEqualSharesTimesPrice()
+        {
+            var trans = new StockTransaction(DateTime.Now, 2.5m, 2);
+            Assert.Equal(5.0m, trans.TotalPrice);
+        }
+    }
+}
