890c1121c55302601d71097aeff4eb99d886e0ce
Raymond Dodge <rrd1907@rit.edu>
2015-10-02 20:09:51 -0400

    Give Stocks/Index some content, and create a stub for Stocks/Search

--- src/SE344/Controllers/StocksController.cs
+++ src/SE344/Controllers/StocksController.cs
@@ -1,14 +1,104 @@
 ﻿using Microsoft.AspNet.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.Data.Entity;
+using SE344.Models;
+using SE344.Services;
+using SE344.ViewModels.Account;
 
 namespace SE344.Controllers
 {
     [Authorize]
     public class StocksController : Controller
     {
-        public IActionResult Index()
+        readonly IStockHistoryService stockHistory = new StubStockHistoryService();
+        readonly IStockInformationService stockInfo = new YahooStockInformationService();
+
+
+        [HttpGet]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index()
+        {
+            var allIds = stockHistory.getKnownIdentifiers();
+            var allStocks = allIds.Select(CreateStock);
+            allStocks.OrderBy(async (a) => (await a).CurrentlyOwned);
+
+            ViewData["stocks"] = allStocks;
+            return View();
+        }
+
+        #region "transaction history"
+/*
+        // POST: /Stock/BuyStock
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Buy(Stock s, int shares)
+        {
+            EnsureDatabaseCreated(_applicationDbContext);
+            return View();
+        }
+
+        [HttpGet]
+        [ValidateAntiForgeryToken]
+        public IActionResult History()
         {
             return View();
+        }
+
+        //???: this is a different page, right?
+        [HttpGet]
+        [ValidateAntiForgeryToken]
+        public IActionResult HistoryCvs()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ClearHistory()
+        {
+            EnsureDatabaseCreated(_applicationDbContext);
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult LoadHistory()
+        {
+            EnsureDatabaseCreated(_applicationDbContext);
+            return View();
+        }
+*/
+        #endregion
+
+        [HttpGet]
+        [ValidateAntiForgeryToken]
+        public IActionResult SearchStocks(String id)
+        {
+            ViewData["result"] = CreateStock(id);
+            return View();
+        }
+
+
+
+        private async Task<Stock> CreateStock(string id)
+        {
+            Stock retVal = new Stock(id, await stockInfo.CurrentPrice(id));
+            retVal.Transactions.AddRange(stockHistory.getTransactions(id));
+            // TODO: retVal.note = ???
+
+            return retVal;
+        }
+
+        private int CompareStockByCurrentHoldings(Stock a, Stock b)
+        {
+            return (a.CurrentPrice * a.CurrentlyOwned).CompareTo(b.CurrentPrice * b.CurrentlyOwned);
         }
     }
 }
