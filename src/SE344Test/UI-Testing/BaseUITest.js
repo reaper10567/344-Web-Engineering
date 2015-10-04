180b9bba68ebdd5126d045a2bbc7d2b35f0cf9f0
Tyler Gerber <tcgerber10567@gmail.com>
2015-10-04 18:03:06 -0400

    first basic UI test.

---
+++ src/SE344Test/UI-Testing/BaseUITest.js
@@ -0,0 +1,12 @@
+var webdriver = require('selenium-webdriver'),
+	By = require('selenium-webdriver').By,
+	until = require('selenium-webdriver').until;
+	
+var driver = new webdriver.Builder()
+	.forBrowser('firefox');
+	.build();
+
+driver.get('http://vm344f.se.rit.edu/R1');
+driver.findElement(By.name('provider')).click();
+driver.wait(until.titleIs('Log into Facebook | Facebook'),1000);
+driver.quit();
