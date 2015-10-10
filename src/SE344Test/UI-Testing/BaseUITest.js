a17defcecb50e39b3e286fec09c2568d712e021a
Raymond Dodge <rrd1907@rit.edu>
2015-10-10 18:57:16 -0400

    switch UI-Test to run against localhost

--- src/SE344Test/UI-Testing/BaseUITest.js
+++ src/SE344Test/UI-Testing/BaseUITest.js
@@ -6,7 +6,9 @@ var driver = new webdriver.Builder()
 	.forBrowser('firefox')
 	.build();
 
-driver.get('http://vm344f.se.rit.edu/R1');
+driver.get('http://localhost:49433/');
+process.stdout.write(driver.getTitle());
+
 driver.findElement(By.name('provider')).click();
 driver.wait(until.titleIs('Log into Facebook | Facebook'),1000);
 driver.quit();
