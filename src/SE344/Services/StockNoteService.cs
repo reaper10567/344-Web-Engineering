4534b92d8d398074df01acf126a8dd5d9a080e15
Raymond Dodge <rrd1907@rit.edu>
2015-11-09 19:17:42 -0500

    Add DbStockNoteService code
    without actually having access to the database. :D

--- src/SE344/Services/StockNoteService.cs
+++ src/SE344/Services/StockNoteService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SE344.Models;
+using System.Data.SqlClient;
 
 namespace SE344.Services
 {
@@ -35,17 +36,58 @@ namespace SE344.Services
             return stock;
         }
         public async Task setNote(Stock stock) {
-            Console.Out.WriteLine(stock.Identifier + ": " + stock.Note);
+            System.Diagnostics.Debug.WriteLine(stock.Identifier + ": " + stock.Note);
         }
     }
 
 /*
     public class DbStockNoteService : IStockNoteService {
         public async Task<Stock> getNote(Stock stock) {
-            
+            SqlCommand query = new SqlCommand(
+                "SELECT content FROM StockNotes WHERE UserID = @UserId AND TickerSymbol = @Symbol"
+            );
+            query.Parameters.AddWithValue("@UserId", ???);
+            query.Parameters.AddWithValue("@Symbol", stock.Identifier);
+
+            var result = query.ExecuteScalarAsync();
+            stock.Note = (await result).ToString();
+            return stock;
         }
-        public async Task<Unit> setNote(Stock stock) {
-            
+        public async Task setNote(Stock stock)
+        {
+            SqlCommand rowToUpdateQuery = new SqlCommand(
+                "SELECT id FROM StockNotes WHERE UserID = @UserId AND TickerSymbol = @Symbol"
+            );
+            rowToUpdateQuery.Parameters.AddWithValue("@UserId", ???);
+            rowToUpdateQuery.Parameters.AddWithValue("@Symbol", stock.Identifier);
+
+            var rowToUpdate = await rowToUpdateQuery.ExecuteScalarAsync();
+
+            SqlCommand updateQuery;
+            if (rowToUpdate == null)
+            {
+                // then no row with specified userid/sumbol combination exists
+                // therefore, create a new row with the data to create
+                updateQuery = new SqlCommand(
+                   "INSERT INTO StockNotes WITH UserID = @UserId AND TickerSymbol = @Symbol AND Content = @Note"
+                );
+                updateQuery.Parameters.AddWithValue("@UserId", ???);
+                updateQuery.Parameters.AddWithValue("@Symbol", stock.Identifier);
+                updateQuery.Parameters.AddWithValue("@Note", stock.Note);
+            }
+            else
+            {
+                // then row with specified userid/sumbol combination exists
+                // therefore, update that row for new note
+                updateQuery = new SqlCommand(
+                    "UPDATE StockNotes SET Contents = @Note WHERE id = @Id"
+                );
+                updateQuery.Parameters.AddWithValue("@Id", rowToUpdate);
+                updateQuery.Parameters.AddWithValue("@Note", stock.Note);
+            }
+
+            var updateQueryResult = updateQuery.ExecuteNonQueryAsync();
+            await updateQueryResult;
         }
     }
 */
