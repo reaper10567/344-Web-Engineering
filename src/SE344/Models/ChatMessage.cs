b3dbde0820050dfa3be3ccefea1cb772fc27435c
Kai <kch2100@rit.edu>
2015-11-09 23:09:57 -0500

    Create Chat model and table migration

---
+++ src/SE344/Models/ChatMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SE344.Models
+{
+    public class ChatMessage
+    {
+        public long Id { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime SentAt { get; set; }
+
+        public string SenderId { get; set; }
+
+        /// <summary>
+        /// Navigation property to get the sender
+        /// </summary>
+        public virtual ApplicationUser Sender { get; set; }
+    }
+}
