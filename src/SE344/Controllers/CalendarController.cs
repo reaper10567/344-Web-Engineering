1fbf67087cb22923e0b9f3771f189691817aaaac
Emily <edf7470@rit.edu>
2015-11-11 16:17:10 -0500

    changes

--- src/SE344/Controllers/CalendarController.cs
+++ src/SE344/Controllers/CalendarController.cs
@@ -2,22 +2,61 @@
 using Microsoft.AspNet.Mvc;
 using System;
 using Microsoft.AspNet.Http.Internal;
+using System.Collections.Generic;
+using SE344.Models;
+using SE344.ViewModels.Calendar;
 
 namespace SE344.Controllers
 {
     [Authorize]
     public class CalendarController : Controller
     {
-        public IActionResult Index()
+        List<CalendarEvent> eventsForUser = new List<CalendarEvent>();
+        public ActionResult Index()
         {
-            return View();
+            //do call to DB here
+            var model = new EventViewModel[eventsForUser.Count+1];
+            for(int i = 0; i < eventsForUser.Count; i++)
+            {
+                model[i] = new EventViewModel
+                {
+                    Title = eventsForUser[i].NameOfEvent,
+                    AllDay = eventsForUser[i].AllDayEvent,
+                    Start = eventsForUser[i].StartTime,
+                    End = eventsForUser[i].EndTime
+                    
+                };
+            }
+            ViewData["Events"] = eventsForUser;
+            //use viewmodel instead of c# object
+            return View(model);
         }
+
+       
 
         [HttpPost]
         public ViewResult Thing(FormCollection form)
         {
             System.Diagnostics.Debug.WriteLine("Posting things here?");
             System.Diagnostics.Debug.WriteLine("Event Name: " + form["Event Name"]);
+
+            bool allDay;
+            Boolean.TryParse(form["allDay"], out allDay);
+            string name = form["Event Name"];
+            DateTime start;
+            DateTime.TryParse(form["StartDateTime"], out start);
+            DateTime end;
+            DateTime.TryParse(form["EndDateTime"], out end);
+            string desc = form["EventDescription"];
+
+            if (allDay) {
+                eventsForUser.Add(new CalendarEvent(name,start,desc));
+            }
+            else
+            {
+                eventsForUser.Add(new CalendarEvent(name,start,end,desc));
+            }
+            System.Diagnostics.Debug.WriteLine("Number Of Events: " + eventsForUser.Count);
             return View("~/Views/Calendar/Index.cshtml");
         }
     }
---
+++ src/SE344/ViewModels/Calendar/EventViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SE344.ViewModels.Calendar
+{
+    public class EventViewModel
+    {
+        public string Title { get; set; }
+        public bool AllDay { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+
+    }
+}
